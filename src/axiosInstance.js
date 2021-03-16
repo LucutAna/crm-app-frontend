b6a3b9f088b40d6b32c5b89510d65766ab042e63
chiriacc <chiriacc@mediamarktsaturn.com>
2021-03-16 12:52:02 +0200

    create separate file for TOKEN and add to git ignore

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,9 +1,10 @@
 import axios from "axios";
+import token from "./token.json"
 
 //add default header with authorization FIFAToken only for development mode
 const setAuth = () => {
     let env = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
-    let FIFAToken = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Im8ybW1zIn0.eyJ0IjoidSIsInN1YiI6IjVlMzJjMWIyNWIyYzVjMDAwYTM2OWM4ZCIsImF1ZCI6ImNybWFwcHRlc3QiLCJzY29wZSI6WyJGSUZBX1BST0ZJTEVfUkVBRCJdLCJpYXQiOjE2MTU3OTkwNTcsImV4cCI6MTYxNjQwMzg1NywiaXNzIjoibW1zaS1kZXYifQ.Rs8iG08sFqJJ8cK4PYyYbPH0_oPPdJrQYSBdekz4dAjP8SpopUW81blbxH9OZOXnhTipviCEM5bWawZhU0sYZJDSlZg8rj3nu-mKNao9vgMU0Qr_MTSZnfXUpYzxDbhlQBucqbvBQnZpJfP3ITOoOEob2t2dktlcr0y-YSrMGH5ehdMpuU2iytx811q4V-AbXP6JsECrrnNxvjNzBNLLXiRkbq5-gInnqmIS-TJeG36h1a4GqsQRI9Y0ENlDQKsV8j8pFyuXeBcIo0E00WXV6R8lTlP5OugvCzzRSU0XmFTQiRnn6faYXVzc-n2gNHWJRI6ozJBLGektkKcYhmSjZA';
+    let FIFAToken = token.FIFAToken;
     return env ? {Authorization: `Bearer ${FIFAToken}`} : '';
 };
 
