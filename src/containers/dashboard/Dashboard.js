13c177c9d2a6ef856c0e99c141ba62d2d3e47abd
chiriacc <chiriacc@mediamarktsaturn.com>
2021-04-07 06:43:28 +0300

    display blank page on dashboard if global object is empty

--- src/containers/dashboard/Dashboard.js
+++ src/containers/dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import Paper from '@material-ui/core/Paper';
 import Grid from "@material-ui/core/Grid";
 import {useContext} from 'react';
-import {find, clone} from 'lodash';
+import {find, clone, isEmpty} from 'lodash';
 
 
 import DashboardStyles from './DashboardStyles';
@@ -20,38 +20,43 @@ const Dashboard = (props) => {
     const classes = DashboardStyles();
     const {customerData} = useContext(GlobalContext);
     const customerLable = ['Club card number', 'Club market', 'Newsletter Sign up', 'Member since', 'Mobile no.'];
+    let customerDetails = [];
     // TODO for birth date
     //moment(customer.birthDate).format(configData.modules.DATE_FORMAT.toUpperCase()),
-    const cusomerDetails = [
-        customerData.cardCiid[0],
-        'MEDIA MARKT ESSEN',
-        permissions(customerData.permissions),
-        customerData.clubDateOfEntry.split('T')[0],
-        customerData.mobile
-    ];
+    if (!isEmpty(customerData))
+        customerDetails = [
+            customerData.cardCiid[0],
+            'MEDIA MARKT ESSEN',
+            permissions(customerData.permissions),
+            customerData.clubDateOfEntry.split('T')[0],
+            customerData.mobile
+        ];
     return (
         <div className={classes.root}>
-            <Paper elevation={3}>
-                <h3 className={classes.paperHeader}>Customer - Club data</h3>
-                <h2 className={classes.customerName}>{`${customerData.salutation} ${customerData.lastName} ${customerData.firstName}`}</h2>
-                <Grid container spacing={1}>
-                    <Grid item xs={12} sm={4}>
-                        <img className={classes.cardImage} src={CardCard} alt="card"/>
+            {!isEmpty(customerData) ?
+            <>
+                <Paper elevation={3}>
+                    <h3 className={classes.paperHeader}>Customer - Club data</h3>
+                    <h2 className={classes.customerName}>{`${customerData.salutation} ${customerData.lastName} ${customerData.firstName}`}</h2>
+                    <Grid container spacing={1}>
+                        <Grid item xs={12} sm={4}>
+                            <img className={classes.cardImage} src={CardCard} alt="card"/>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            {customerLable.map(lable => <h4 key={lable}>{lable}</h4>)}
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            {customerDetails.map(info => <h4 key={info} className={classes.cusomerDetails}>{info}</h4>)}
+                        </Grid>
                     </Grid>
-                    <Grid item xs={12} sm={4}>
-                        {customerLable.map(lable => <h4 key={lable}>{lable}</h4>)}
-                    </Grid>
-                    <Grid item xs={12} sm={4}>
-                        {cusomerDetails.map(info => <h4 key={info} className={classes.cusomerDetails}>{info}</h4>)}
-                    </Grid>
-                </Grid>
-            </Paper>
-            <Paper elevation={3}>
-                <h3 className={classes.paperHeader}>Purchases history</h3>
-            </Paper>
-            <Paper elevation={3}>
-                <h3 className={classes.paperHeader}>Coupons</h3>
-            </Paper>
+                </Paper>
+                <Paper elevation={3}>
+                    <h3 className={classes.paperHeader}>Purchases history</h3>
+                </Paper>
+                <Paper elevation={3}>
+                    <h3 className={classes.paperHeader}>Coupons</h3>
+                </Paper>
+            </> : null}
         </div>
     )
 }
