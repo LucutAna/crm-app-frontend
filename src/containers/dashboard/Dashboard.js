ee925ae66f5e9bca176f196a49ca1d114e11edd4
chiriacc <chiriacc@mediamarktsaturn.com>
2021-04-09 20:18:22 +0300

    creat sales transction service

--- src/containers/dashboard/Dashboard.js
+++ src/containers/dashboard/Dashboard.js
@@ -1,13 +1,16 @@
 import Paper from '@material-ui/core/Paper';
 import {useContext, useState, useEffect} from 'react';
 import moment from 'moment';
-import {find, clone, isEmpty} from 'lodash';
+import {find, clone, isEmpty, flatten, concat, each, pullAt, values, filter, isNil, orderBy} from 'lodash';
 
 
 import DashboardStyles from './DashboardStyles';
 import {GlobalContext} from '../../context/GlobalState';
 import ConfigService from './../../shared/services/ConfigService';
-import CustomerDataInfo from './../../components/customerDataInfo/CustomerDataInfo'
+import CustomerDataInfo from './../../components/customerDataInfo/CustomerDataInfo';
+import HistoryPurchases from './../../components/historyPurchases/HistoryPurchases';
+import SalesTransactions from './../../shared/services/SalesTransactions';
+
 
 const permissions = (customerPermision) => {
     const permissionsKey = find(Object.keys(customerPermision), (key) => {
@@ -21,6 +24,94 @@ const Dashboard = ({configData}) => {
     const classes = DashboardStyles();
     const {customerData} = useContext(GlobalContext);
     const [customerInfo, setCustomerInfo] = useState('');
+    const [salesOrderHistory, setSalesOrderHistory] = useState('')
+
+    const showSalesOrderHistory = (ordersResponse) => {
+        let orderHistory = flatten(ordersResponse[0]);
+        let orderHistoryOnline = ordersResponse[1];
+
+        let orderHistoryFom = [];
+        if (ordersResponse.length > 2)
+            orderHistoryFom = ordersResponse[2];
+
+        const allOrders = concat(orderHistory, orderHistoryOnline, orderHistoryFom);
+        // if (allOrders.length === 0) {
+        //     $scope.customer.showOrdersSpinner = false;
+        //     return;
+        // }
+
+        let filteredOrders = {};
+        each(allOrders, function (myOrder, i) {
+            if (myOrder.type === 'online') {
+                if (!filteredOrders[myOrder.orderNumber]) {
+                    filteredOrders[myOrder.orderNumber] = i;
+                }
+            } else if (myOrder.type === 'FOM') {
+                filteredOrders[`${myOrder.orderNumber}_${myOrder.orderOutletId}}`] = i;
+            } else {
+                filteredOrders[`${myOrder.orderNumber}_${myOrder.orderOutletId}_${myOrder.date.getTime()}`] = i;
+            }
+
+            each(myOrder.orderDetails, function (details) {
+                if (details.itemSet !== undefined) {
+                    each(details.itemSet, function (item) {
+                        myOrder.orderDetails.push(item);
+                    });
+                    myOrder.orderDetails.splice(i, 1);
+                }
+            });
+        });
+
+        const deduplicatedOrders = pullAt(allOrders, values(filteredOrders));
+        const orders = filter(deduplicatedOrders, function (o) {
+            return o.orderGrossTotal >= 0;
+        });
+        setSalesOrderHistory(orderBy(orders, ['date', 'orderNumber'], ['desc', 'desc']));
+        //$scope.customer.showOrdersSpinner = false;
+    };
+
+    useEffect(() => {
+        if (!isEmpty(customerData)) {
+            const initializeTransactionsPanel = () => {
+                let partyUUID = customerData.partyUid;
+                let wcsUserId = customerData.wcsUserId || "";
+                let orderHistorySettings = configData.orderHistorySettings;
+                let getOrdersFromCar = true;
+                let getOrdersFromFom = false;
+                if (orderHistorySettings) {
+                    getOrdersFromCar = orderHistorySettings.getOrdersFromCar;
+                    getOrdersFromFom = orderHistorySettings.getOrdersFromFom;
+                }
+                let ciidPromises = [];
+
+                if (customerData.cardCiid) {
+                    customerData.cardCiid.forEach(ciid => {
+                        ciidPromises.push(SalesTransactions.getSalesOrderHistoryAtStore(ciid));
+                        ciidPromises.push(SalesTransactions.getSalesOrderHistoryOnlineByCiid(ciid));
+                    });
+                }
+
+                if (!isNil(customerData.ccrCiid)) {
+                    ciidPromises.push(SalesTransactions.getSalesOrderHistoryAtStore(customerData.ccrCiid));
+                    ciidPromises.push(SalesTransactions.getSalesOrderHistoryOnlineByCiid(customerData.ccrCiid));
+                }
+
+                Promise.all([
+                    // firing all in store orders calls
+                    Promise.all(ciidPromises),
+                    // firing the online orders from car call if enabled
+                    getOrdersFromCar ? SalesTransactions.getSalesOrderHistory(partyUUID) : [],
+                    // firing the online orders from fom call if enabled
+                    getOrdersFromFom ? SalesTransactions.getSalesOrderHistoryFom(configData, wcsUserId) : []
+                ]).then(result => {
+                    showSalesOrderHistory(result);
+                }).catch((error) => {
+                    console.log(error);
+                });
+            };
+            initializeTransactionsPanel();
+        }
+    }, [customerData, configData])
 
     useEffect(() => {
         if (!isEmpty(customerData)) {
@@ -44,16 +135,14 @@ const Dashboard = ({configData}) => {
                 console.log(error);
             }
         }
-    }, [customerData, configData])
-
+    }, [customerData, configData]);
+    
     return (
         <div className={classes.root}>
             {!isEmpty(customerData) ?
                 <>
                     <CustomerDataInfo customer={customerInfo}/>
-                    <Paper elevation={3}>
-                        <h3 className={classes.paperHeader}>Purchases history</h3>
-                    </Paper>
+                    <HistoryPurchases salesOrderHistory={salesOrderHistory} configData={configData}/>
                     <Paper elevation={3}>
                         <h3 className={classes.paperHeader}>Coupons</h3>
                     </Paper>
---
+++ src/shared/services/SalesTransactions.js
@@ -0,0 +1,123 @@
+import axiosInstance from '../../axiosInstance';
+import ConfigService from './ConfigService';
+import {isEmpty} from 'lodash';
+
+const getSalesOrderHistoryAtStore = async (loyaltyNumber) => {
+    const result = await axiosInstance.get(`/crm-sales/orderHistory/${loyaltyNumber}`);
+    if (!result || result.status !== 200)
+        return [];
+    result.data
+        .map(sale => {
+            sale.date = new Date(sale.orderTime);
+            sale.type = 'store';
+            sale.isNavCollapsed = false;
+            sale.loyaltyNumber = loyaltyNumber;
+
+            const getStoreDetails = async () => {
+                const storeConfig = await ConfigService.getStore(sale.orderOutletId);
+                sale.storeName = storeConfig.data.storeName;
+                sale.storeSapId = storeConfig.data.sapCode;
+            };
+            getStoreDetails();
+            return sale;
+        });
+    return result.data;
+};
+
+const getSalesOrderHistoryOnlineByCiid = async (loyaltyNumber) => {
+    const result = await axiosInstance.get(`/crm-sales/salesOrder/orderHistoryOnline/${loyaltyNumber}`);
+    if (!result || result.status !== 200)
+        return [];
+    result.data
+        .map(sale => {
+            sale.date = new Date(sale.orderTime);
+            sale.type = 'online';
+            sale.isNavCollapsed = false;
+            sale.loyaltyNumber = loyaltyNumber;
+
+            const getStoreDetails = async () => {
+                const storeConfig = await ConfigService.getStore(sale.orderOutletId);
+                sale.storeName = storeConfig.data.storeName;
+                sale.storeSapId = storeConfig.data.sapCode;
+            };
+            getStoreDetails();
+            return sale;
+        });
+    return result.data;
+}
+
+const getSalesOrderHistory = async (partyUUID) => {
+    const result = await axiosInstance.get(`/crm-sales/orderHistoryOnline/${partyUUID}`);
+    if (!result || result.status !== 200)
+        return [];
+    result.data
+        .map(sale => {
+            sale.date = new Date(sale.orderTime);
+            sale.type = 'online';
+            sale.isNavCollapsed = false;
+
+            const getStoreDetails = async () => {
+                const storeConfig = await ConfigService.getStore(sale.orderOutletId);
+                sale.storeName = storeConfig.data.storeName;
+                sale.storeSapId = storeConfig.data.sapCode;
+            };
+            getStoreDetails();
+            return sale;
+        });
+    return result.data;
+};
+
+const getSalesOrderHistoryFom = async ({configData, wcsUserId}) => {
+    //TODO check if it work
+    if (!isEmpty(wcsUserId)) {
+        let deferred = new Promise();
+        deferred.resolve([]);
+        return deferred.promise;
+    }
+    try {
+        const result = await axiosInstance.get(`/crm-sales/orderHistoryFom/${wcsUserId}`, {
+            headers: {
+                "salesDivision": configData.salesDivision,
+                "subsidiary": configData.subsidiary
+            }
+        });
+        result.data
+            .map(sale => {
+                if (sale.orderTime)
+                    sale.date = new Date(sale.orderTime);
+
+                sale.type = 'FOM';
+                sale.isNavCollapsed = false;
+                // map webshops for FOM
+                if (!isEmpty(sale.outletId)) {
+                    sale.storeName = "MEDIA MARKT ONLINE SHOP";
+                    let orderHistorySettings = configData.orderHistorySettings;
+                    if (orderHistorySettings) {
+                        sale.storeSapId = orderHistorySettings.fomSettings.onlineStoreSapId;
+                    } else {
+                        sale.storeSapId = "";
+                    }
+                    return sale;
+                }
+                const getStoreDetails = async () => {
+                    const storeConfig = await ConfigService.getStore(sale.orderOutletId);
+                    sale.storeName = storeConfig.data.storeName;
+                    sale.storeSapId = storeConfig.data.sapCode;
+                };
+                getStoreDetails();
+                return sale;
+            });
+        return result.data;
+    } catch (error) {
+        return []
+    }
+};
+
+const SalesTransactions = {
+    getSalesOrderHistoryAtStore,
+    getSalesOrderHistoryOnlineByCiid,
+    getSalesOrderHistory,
+    getSalesOrderHistoryFom
+};
+
+export default SalesTransactions;
